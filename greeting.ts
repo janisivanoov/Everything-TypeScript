88a6c09f9e1d8bff1e0e9e85f25af651742cbaa4
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-03-01 10:36:49 +0200

    Add files via upload

---
+++ greeting.ts
@@ -0,0 +1,11 @@
+var Greeting = (function () {
+    function Greeting() {
+    }
+    Greeting.prototype.greet = function () {
+       console.log("Hello World!!!");
+    };
+     return Greeting;
+ }());
+ 
+ var obj = new Greeting();
+ obj.greet()
